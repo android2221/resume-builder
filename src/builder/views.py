edc92b8e107be9667c9da6a977866f2ed64b34d8
android2221 <android2221@gmail.com>
2019-12-28 20:26:11 -0700

    rendering simple templates

--- src/builder/views.py
+++ src/builder/views.py
@@ -1,10 +1,23 @@
+# from django.shortcuts import render
+# from django.http import HttpResponse
+
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the homepage, which is from the builder app.")
+
+# def builder(request):
+#     return HttpResponse("Hello, world. You're at the builder index.")
+
+
 from django.shortcuts import render
-from django.http import HttpResponse
+#from .models import Question
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the homepage, which is from the builder app.")
+    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'some_sample_text': 'some sample i typed'}
+    return render(request, 'builder/index.html', context)
 
 def builder(request):
-    return HttpResponse("Hello, world. You're at the builder index.")
-
-
+    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'some_sample_text': 'some sample i typed'}
+    return render(request, 'builder/builder.html', context)
