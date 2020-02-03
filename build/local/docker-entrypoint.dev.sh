cc2f54cfb91d85d3d9d5b3c550606551a25cceb1
android2221 <android2221@gmail.com>
2020-02-03 16:17:16 -0700

    local development stack working

--- build/local/docker-entrypoint.dev.sh
+++ build/local/docker-entrypoint.dev.sh
@@ -4,6 +4,10 @@ echo "Apply database migrations"
 python manage.py makemigrations accounts builder
 python manage.py migrate
 
+# Collect static
+echo "Collectng static fles"
+python manage.py collectstatic --noinput
+
 # Start server
 echo "Starting server"
 python manage.py runserver 0.0.0.0:8001
