a970b6d4834ba5ff43c5dba40c44f71051cdeb81
android2221 <android2221@gmail.com>
2020-02-10 22:19:28 -0700

    hacky working tint mce, no saving

--- webapp/builder/forms.py
+++ webapp/builder/forms.py
@@ -3,8 +3,11 @@ from django import forms
 from django.contrib.auth.models import User
 
 class ResumeEditorForm (forms.Form):
-     content = forms.CharField(widget=forms.Textarea)
+    content = forms.CharField(widget=forms.Textarea)
 
+    def __init__(self, *args, **kwargs):
+        super(ResumeEditorForm, self).__init__(*args, **kwargs)
+        self.fields['content'].widget.attrs={'class': "resume-editor"}
 class ActivateResumeForm (forms.Form):
     profile_active = forms.BooleanField(required=False, label="")
 
