9d07968fbde9259e8f4df922ff9a420cc25e9424
android2221 <android2221@gmail.com>
2020-02-12 22:46:57 -0700

    styling on resume activate toggle

--- webapp/builder/urls.py
+++ webapp/builder/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('builder/', views.load_builder, name='load_builder'),
     path('save-builder/', views.save_builder, name='save_builder'),
-    path('preview-resume/', views.preview_resume, name='preview_resume'),
+    path('builder/preview/', views.preview_resume, name='preview_resume'),
     path('activate-resume/', views.toggle_resume_active, name='activate-resume'),
 ]
