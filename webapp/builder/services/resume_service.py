989667c150495433aaea0878d5cbe201a74c06ca
Andy Hablewitz <mail@andyhablewitz.com>
2020-05-14 21:20:46 -0600

    wip - POSTing resume forms through service, starting to work on GETting

--- webapp/builder/services/resume_service.py
+++ webapp/builder/services/resume_service.py
@@ -8,7 +8,6 @@ class ResumeService():
 
     def get_resume_by_profile_url(self, request_profile_url):
         try:
-            accounts = Account.objects.filter(profile_url=request_profile_url)
             account = Account.objects.get(profile_url=request_profile_url)
             if account.user.resume.is_live is True:
                 return account.user.resume
@@ -22,32 +21,92 @@ class ResumeService():
     def get_resume_education_by_id(self, resume_id):
         return ResumeEducation.objects.filter(resume=resume_id)
 
-    def save_resume(self, resume, payload):
-        posted_resume_jobs_section_title_form = ResumeJobsSectionTitleForm(payload)
-        posted_resume_education_section_title_form = ResumeEducationSectionTitleForm(payload)
-        try:
-            resume_details_result = self.save_resume_details(resume, payload)
-            save_results = {
-                "resume_details_result": resume_details_result
-            }
+    def save_resume(self, user, post_payload):
+        resume_details_form = self.process_resume_detail_form(post_payload, user.resume)
+        resume_jobs_formset = self.process_resume_jobs_formset(post_payload, user.resume)
+        resume_education_formset = self.process_resume_education_formset(post_payload, user.resume)
+        resume_jobs_section_title_form = self.process_resume_section_title_form(post_payload, user.resume)
+        forms = {
+            'resume_details_form': resume_details_form,
+            'resume_jobs_formset': resume_jobs_formset,
+            'resume_education_formset': resume_education_formset,
+            'resume_jobs_section_title_form': resume_jobs_section_title_form,
+        }
+        user.resume.save()
+        return forms
+
+    def init_resume_detail_form_data(self, resume):
+        return { 
+            'resume_title': resume.resume_title,
+            'contact_information_section_title': resume.contact_information_section_title,
+            'contact_information': resume.contact_information,
+            'personal_statement_section_title': resume.personal_statement_section_title,
+            'personal_statement': resume.personal_statement,
+            'current_skills_section_title': resume.current_skills_section_title,
+            'current_skills': resume.current_skills
+        }
+    
+    def init_resume_jobs_section_title_form(self, resume):
+        return {'resume_jobs_section_title': resume.resume_jobs_section_title}
+    
+    def init_resume_education_section_title_form(self, resume):
+        return {'resume_education_section_title': resume.resume_education_section_title}
+        
+    def process_resume_education_section_title_form(self, post_payload, resume):
+        form = ResumeEducationSectionTitleForm(post_payload)
+        if form.is_valid():
+            resume.resume_education_section_title = form.cleaned_data.get('resume_education_section_title')
+            return ResumeEducationSectionTitleForm(self.init_resume_education_section_title_form)
+        else:
+            return form
+
+    def process_resume_section_title_form(self, post_payload, resume):
+        form = ResumeJobsSectionTitleForm(post_payload)
+        if form.is_valid():
+            resume.resume_jobs_section_title = form.cleaned_data.get('resume_jobs_section_title')
+            return ResumeJobsSectionTitleForm(self.init_resume_jobs_section_title_form(resume))
+        else:
+            return form
+
+    def process_resume_education_formset(self, post_payload, resume):
+        forms = ResumeEducationFormset(post_payload, prefix='resume_education')
+        if forms.is_valid():
+            for form in forms:
+                if form.has_changed():
+                    resume_education = form.save(commit=False)
+                    resume_education.resume = resume
+                    resume_education.save()
+            return ResumeEducationFormset(queryset=ResumeEducation.objects.filter(resume=resume.pk), prefix='resume_education')
+        else:    
+            return forms
+
+    def process_resume_jobs_formset(self, post_payload, resume):
+        forms = ResumeJobsFormset(post_payload, prefix='resume_job')
+        if forms.is_valid():  
+            for form in forms:
+                if form.has_changed():
+                    resume_job = form.save(commit=False)
+                    resume_job.resume = resume
+                    resume_job.save()
+            return ResumeJobsFormset(queryset=ResumeJob.objects.filter(resume=resume.pk), prefix='resume_job')
+        else:
+            return forms
+        
+    def process_resume_detail_form(self, post_payload, resume):
+        resume_detail_form_data = self.init_resume_detail_form_data(resume)
+        form = ResumeDetailsForm(post_payload)
+        if form.is_valid():
+            resume.resume_title=form.cleaned_data.get('resume_title')
+            resume.contact_information_section_title=form.cleaned_data.get('contact_information_section_title')
+            resume.contact_information=form.cleaned_data.get('contact_information')
+            resume.personal_statement_section_title=form.cleaned_data.get('personal_statement_section_title')
+            resume.personal_statement=form.cleaned_data.get('personal_statement')
+            resume.current_skills_section_title=form.cleaned_data.get('current_skills_section_title')
+            resume.current_skills=form.cleaned_data.get('current_skills')
             resume.save()
-        except Exception as ex:
-            return False
-        return save_results
-
-        #     if posted_resume_jobs_section_title_form.is_valid():
-        #         form = posted_resume_jobs_section_title_form
-        #         resume.resume_jobs_section_title = form.cleaned_data.get('resume_jobs_section_title')
-
-        #     if posted_resume_education_section_title_form.is_valid():
-        #         form = posted_resume_education_section_title_form
-        #         resume.resume_education_section_title = form.cleaned_data.get('resume_education_section_title')
-                
-        #     resume.save()
-        #     return True
-        # except Exception as ex:
-        #     return False           
-        # return False
+            return ResumeDetailsForm(resume_detail_form_data)
+        else:
+            return form
 
     def preview_resume(self, payload):
         posted_form = ResumeEditorForm(payload)
