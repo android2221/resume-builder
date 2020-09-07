0de744d83ee9f78a5265713857b3b714687791f0
Andy Hablewitz <mail@andyhablewitz.com>
2020-09-07 16:26:08 -0600

    Feat/prevent remove print watermark (#12)
    * print watermark enforced w/ JS
    
    * terms and conditions
    
    * remove print watermark - it's free :)
    
    * terms and conditions

--- webapp/builder/urls.py
+++ webapp/builder/urls.py
@@ -1,9 +1,12 @@
 from builder import views
 from django.urls import path, reverse_lazy
 from django.views.generic import FormView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('terms/', TemplateView.as_view(template_name="builder/terms.html"), name="terms"),
+    path('privacy/', TemplateView.as_view(template_name="builder/privacy.html"), name="privacy"),
     path('builder/', views.builder_page, name='builder_page'),
     path('builder/preview/', views.preview_resume, name='preview_resume'),
     path('builder/publish/', views.publish_resume, name='publish_resume'),
